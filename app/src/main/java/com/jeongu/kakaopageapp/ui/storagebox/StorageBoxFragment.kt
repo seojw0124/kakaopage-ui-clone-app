881a0b57543accd08c9c7829cad331bc011b700e
seojw0124 <wjddn2000124@gmail.com>
2024-07-31 23:01:15 +0900

    feat: 보관함 아이템 클릭 시 상세화면 이동

--- app/src/main/java/com/jeongu/kakaopageapp/ui/storagebox/StorageBoxFragment.kt
+++ app/src/main/java/com/jeongu/kakaopageapp/ui/storagebox/StorageBoxFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.jeongu.kakaopageapp.databinding.FragmentStorageBoxBinding
 
 class StorageBoxFragment : Fragment() {
@@ -19,7 +20,8 @@ class StorageBoxFragment : Fragment() {
     }
     private val storageBoxAdapter by lazy {
         StorageBoxListAdapter { content ->
-            Toast.makeText(requireContext(), content.thumbnailImage, Toast.LENGTH_SHORT).show()
+            val action = StorageBoxFragmentDirections.actionGlobalArticleDetail(content.id)
+            findNavController().navigate(action)
         }
     }
 
